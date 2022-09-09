fc7e8f4f2fc17fe89bdc3778d5866d092472f491
leem919 <leem919@gmail.com>
2022-09-08 18:00:43 -0700

    Uploaded main files

---
+++ serviceworker.js
@@ -0,0 +1,20 @@
+var staticCacheName = "pwa";
+const version = 10;
+
+self.addEventListener("install", function (e) {
+e.waitUntil(
+	caches.open(staticCacheName).then(function (cache) {
+	return cache.addAll(["/xcloud-pwa"]);
+	})
+);
+});
+
+self.addEventListener("fetch", function (event) {
+console.log(event.request.url);
+
+event.respondWith(
+	caches.match(event.request).then(function (response) {
+	return response || fetch(event.request);
+	})
+);
+});
